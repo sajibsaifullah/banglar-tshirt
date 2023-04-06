11e5718cb4dfe6819d33f2aa05714c9fd4024374
sajibsaifullah <dusajibphy@gmail.com>
2023-04-07 01:49:25 +0600

    cart added

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css';
 
 const Home = () => {
     const tshirts = useLoaderData();
-    console.log(tshirts);
+    // console.log(tshirts);
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = tshirt => {
+        const exist = cart.find(ts => ts._id === tshirt._id);
+        if(exist){
+            alert('aaa')
+        }
+        else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+        
+    }
+
+    const handleRemoveFromCart = id =>{
+        const remainingCart = cart.filter(tshirt => tshirt._id !== id)
+        setCart(remainingCart)
+    }
+
     return (
-        <div>
-            <h2>This is Home: {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+                {
+                    tshirts.map(tshirt => <TShirt
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></TShirt>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart 
+                cart={cart}
+                handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
+            </div>
         </div>
     );
 };
